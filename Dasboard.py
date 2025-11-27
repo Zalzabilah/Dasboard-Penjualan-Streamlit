4979de4a6600235d7ff8c135a73a25a14164519b
Zalzabilah <143878659+Zalzabilah@users.noreply.github.com>
2025-11-27 20:21:31 +0700

    Update Dasboard.py

--- Dasboard.py
+++ Dasboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sckit as skl
+
 
 # Coba import plotly.express, jika gagal, gunakan alternatif atau pesan error
 try:
@@ -293,3 +293,4 @@ elif page == "Prediksi":
                         st.write("**Akurasi Model**: Tidak dapat dihitung karena data terlalu sedikit.")
                 except Exception as e:
                     st.error(f"Terjadi kesalahan saat melakukan prediksi: {str(e)}")
+
